8e9cf9f0ad1df3de3b42f343a95c6343856d1df1
Coopex-entregas <coopexentregas.rn@gmail.com>
2025-07-02 18:50:35 -0300

    Versão final simplificada para deploy no Render

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,3 @@
-# wsgi.py
-from app import app
-
-if __name__ == "__main__":
-    # Este comando é para o Render iniciar a aplicação
-    # O host 0.0.0.0 é necessário para ser visível externamente
-    # A porta é fornecida pelo Render através da variável de ambiente PORT
-    app.run(host="0.0.0.0")
+ if __name__ == "__main__":
+     app.run()
+ ```
