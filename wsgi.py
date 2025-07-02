04b1663427f31a6d9d0cc7d6db0b1b8e0dbbf7a3
Coopex-entregas <coopexentregas.rn@gmail.com>
2025-07-02 18:33:36 -0300

    Ajusta wsgi.py e usa python para iniciar

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
+# wsgi.py
 from app import app
 
 if __name__ == "__main__":
-    app.run()
+    # Este comando é para o Render iniciar a aplicação
+    # O host 0.0.0.0 é necessário para ser visível externamente
+    # A porta é fornecida pelo Render através da variável de ambiente PORT
+    app.run(host="0.0.0.0")
